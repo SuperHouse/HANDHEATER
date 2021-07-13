1cc8140e3d71716855532cf123b29c806a210777
Jonathan Oxer <jon@oxer.com.au>
2021-07-13 16:56:50 +1000

    Begin restructure of example firmware.

--- Firmware/HandHeaterFirmwareESP32/config.h-example.h
+++ Firmware/HandHeaterFirmwareESP32/config.h-example.h
@@ -1,35 +1,52 @@
-// Your SSID/Password:
+/* ----------------- General config -------------------------------- */
+/* WiFi */
 #define WIFI_SSID       "YOUR SSID"
 #define WIFI_PASSWORD   "YOUR PSK"
 
-// Your MQTT Broker IP address:
-#define MQTT_BROKER     "192.168.1.111"
+/* MQTT */
+const char* mqtt_broker           = "192.168.1.111"; // IP address of your MQTT broker
+const char* mqtt_username         = "";              // Your MQTT username
+const char* mqtt_password         = "";              // Your MQTT password
 
-// All messages via Bluetooth must be prepended with this key
-#define BT_PASSWORD     "MyKey"
+/* Serial */
+#define     SERIAL_BAUD_RATE    115200               // Speed for USB serial console
 
-// Activate features
-#define ENABLE_WIFI       true
-#define ENABLE_MQTT       true
-#define ENABLE_BLUETOOTH  false
-#define ENABLE_CAN        true
+/* Bluetooth */
+#define BT_PASSWORD     "MyKey"    // All messages must be prepended with this key
 
-#define TEMPERATURE_OFFSET -7
-#define FIRMWARE_VERSION    3
+/* Activate features */
+#define ENABLE_WIFI           true
+#define ENABLE_MQTT           true
+#define ENABLE_BLUETOOTH      false
+#define ENABLE_CAN            true
 
-#define FAN_COLD_SPEED    80
-#define FAN_WARM_SPEED   100
-#define FAN_HOT_SPEED    140
+/* Heater parameters */
+#define FAN_COLD_SPEED          80
+#define FAN_WARM_SPEED         100
+#define FAN_HOT_SPEED          140
+#define TEMPERATURE_WARM        80
+#define TEMPERATURE_HOT        120
+#define TEMPERATURE_SAFE_LIMIT 130   // Maximum safe output temperature in Celsius
 
-#define TEMPERATURE_WARM  80
-#define TEMPERATURE_HOT   120
+/* ----------------- Hardware-specific config ---------------------- */
+/* Temperature sensor */
+#define BCOEFFICIENT          3380   // Thermistor beta coefficient
+#define TEMPERATURE_OFFSET      -7   // Correction for offset
+#define FAN_TACHO_PROCESS_INTERVAL  0.5  // How often to process accumulated fan tacho pulses, in seconds
 
-// Maximum safe output temperature in Celsius
-#define TEMPERATURE_SAFE_LIMIT 130
+#define FAN_FREQUENCY        15000   // PWM frequency for fan control
+#define FAN_CHANNEL              0   // PWM output channel for fan
+#define FAN_RESOLUTION           8   // PWM resolution for fan
+#define HEATER_FREQUENCY      5000   // PWM frequency for heater control
+#define HEATER_CHANNEL           1   // PWM output channel for heater
+#define HEATER_RESOLUTION        8   // PWM resolution for heater
 
-/** To Do
- *  - Simple setup of WiFi config. Maybe AP mode?
- *  - Factory reset mode. Perhaps hold down button while powering up?
- *  - Bluetooth security? Allows any device to pair. Perhaps prepend all
- *     control messages with a unique key (PSK)
- */
+// Pin definitions
+#define CAN_TX_PIN       GPIO_NUM_5
+#define CAN_RX_PIN       GPIO_NUM_4
+#define BUTTON_PIN       GPIO_NUM_14
+#define HEATER_PIN       GPIO_NUM_16  // PWM
+#define FAN_PIN          GPIO_NUM_17  // PWM
+#define STATUS_LED_PIN   GPIO_NUM_25
+#define TEMP_SENSOR_PIN  GPIO_NUM_32
+#define FAN_TACHO_PIN    GPIO_NUM_33
